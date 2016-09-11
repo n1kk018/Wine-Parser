0af6b20659c4fab6ad6f468e732aaca82c16ccc6
nikko <nikko@nikko-laptop>
2016-09-12 00:04:16 +0200

    dsg

--- src/main/java/fr/afcepf/atod/wine/data/parser/XmlParser.java
+++ src/main/java/fr/afcepf/atod/wine/data/parser/XmlParser.java
@@ -51,11 +51,11 @@ public class XmlParser {
     private static Map<String,ProductVintage> vintages = new HashMap<String,ProductVintage>();
     private static java.util.List<ProductWine> list = new ArrayList<ProductWine>();
     private static String apiBaseUrl = "http://services.wine.com/api/beta2/service.svc/xml/";
-    private static String apikey = "37662dd9dbf72936b590e8bdec649a30";
+    private static String apikey = "";
 
     public static void main(String[] args) {
         log.info("\t ### debut du test ###");
-        /*URL url;
+        URL url;
 		try {
 			url = new URL(apiBaseUrl+"/categorymap?filter=categories(490)&apikey="+apikey); 
         	File file = new File("/FilesXML/Wines/categoryMap.xml");
@@ -63,7 +63,7 @@ public class XmlParser {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
         
 
         BeanFactory bf = new ClassPathXmlApplicationContext("classpath:springData.xml");
